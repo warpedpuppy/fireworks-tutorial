50d7010a95f9d0a13ad13b1f39b02060df986cd1
ted <ted@warpedpuppy.com>
2023-08-28 11:09:36 -0400

    converted to oop

--- canvas.js
+++ canvas.js
@@ -1,55 +1,75 @@
 (function () {
 
+
+
 	const canvas = document.getElementById('canvas');
 	canvas.width = 600;
 	canvas.height = 300;
 	const context = canvas.getContext('2d');
-	let centerX =  canvas.width / 2;
-	let speed = 10;
-	let deacceleration = 0.95;
-	const centerY = canvas.height / 2;
-	const radius = 5;
-	let alpha = 1;
-	
-	drawCircle(centerX)
 
-	function drawCircle(centerX, centerY, alpha) {
-		context.beginPath();
-		context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-		context.fillStyle = `rgba(0, 0, 0, ${alpha})`;
-		context.fill();
-	}
+	class Firework {
+		beamQ = 10;
+		beams = [];
+		centerX = 0.1;
+		centerY = canvas.height / 2;
+		radius = 0;
+		angle = 0;
+		increase;
+		constructor(radius) {
+			this.radius = radius;
+			for (let i = 0; i < this.beamQ; i++) {
+				let beam = new Beam(this.radius);
+				this.beams.push(beam);
+			}
+		}
 
-	function distributeAroundRadial (array, radius, addTo, spiral, itemQ) {
-		var arrayQ = (itemQ === undefined)?array.length:itemQ;
-		var increase = (Math.PI * 2)/arrayQ;
-		var  angle = 0;
-		for( var i = 0; i < arrayQ; i++ ) {
-			var item = array[i];
-			item.rotation = 0;
-			item.x =  radius * Math.cos( angle ) ;
-			item.y =  radius * Math.sin( angle ) ;
-			angle += increase;
-			addTo.addChild(item);
-			if(spiral === true)radius +=3;
+		returnXY(radius, angle) {
+			return {x: (canvas.width / 2) + radius * Math.cos( this.angle ), y: (canvas.height / 2) + radius * Math.sin( angle ) }
+		}
+
+		reset() {
+			this.angle = 0;
+			this.radius = 0;
+		}
+
+		draw() {
+			for (let i = 0; i < this.beamQ; i++) {
+				let increase = (Math.PI * 2) / this.beamQ;
+				this.angle += increase;
+				let { x, y } = this.returnXY(this.radius, this.angle);
+				this.beams[i].draw(x,y);
+				this.radius += 0.25;
+				if (this.radius > Math.max(canvas.width, canvas.height) / 2) {
+					this.reset();
+				}
+			}
 		}
 	}
 
+	class Beam {
 
+		color = "#FF00FF";
+		size;
+		constructor (size) {
+			this.size = size;
+		}
+
+		draw(x, y) {
+			context.beginPath();
+			context.arc(x, y, this.size, 0, 2 * Math.PI, false);
+			context.fillStyle = this.color;
+			context.fill();
+		}
+	}
+
+	const firework = new Firework(5);
 	function animate () {
 		context.clearRect(0,0,canvas.width,canvas.height)
-		centerX += speed;
-		speed *= deacceleration;
-		alpha *= deacceleration;
-		if (speed < 0.1) {
-			centerX = canvas.width / 2;
-			speed = 8;
-			alpha = 1;
-		}
-
-		drawCircle(centerX, centerY, alpha);
+		firework.draw();
 		requestAnimationFrame(animate)
 	}
 	animate();
+
+
 
 })()
