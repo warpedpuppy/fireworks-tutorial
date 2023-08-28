07ba87042a022c108081ebc11c7137ff28b88474
ted <ted@warpedpuppy.com>
2023-08-28 10:39:04 -0400

    cleaned step_one

--- canvas.js
+++ canvas.js
@@ -4,24 +4,59 @@
 	canvas.width = 600;
 	canvas.height = 300;
 	const context = canvas.getContext('2d');
-	let centerX = 0.1;
-	const centerY = canvas.height / 2;
-	const radius = 10;
-	
-	drawCircle(centerX)
 
-	function drawCircle(centerX) {
-		context.beginPath();
-		context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-		context.fillStyle = 'green';
-		context.fill();
+	class Firework {
+		beamQ = 1;
+		beams = [];
+		centerX = 0.1;
+		centerY = canvas.height / 2;
+		radius;
+		constructor(radius) {
+			this.radius = radius;
+			for (let i = 0; i < this.beamQ; i++) {
+				let beam = new Beam(this.centerX, this.centerY, this.radius);
+				this.beams.push(beam);
+			}
+		}
+
+		draw() {
+			for (let i = 0; i < this.beamQ; i++) {
+				this.beams[i].draw();
+			}
+		}
 	}
 
+	class Beam {
 
+		color = "#FF00FF";
+		radius;
+		centerX; 
+		centerY;
+		speed = 10;
+		constructor (centerX, centerY, radius) {
+			this.centerX = centerX;
+			this.centerY = centerY;
+			this.radius = radius;
+		}
+
+		draw() {
+			this.centerX += this.speed;
+			this.speed *= 0.99;
+			if (this.centerX > canvas.width) {
+				this.speed = 10;
+				this.centerX = 0;
+			}
+			context.beginPath();
+			context.arc(this.centerX, this.centerY, this.radius, 0, 2 * Math.PI, false);
+			context.fillStyle = 'green';
+			context.fill();
+		}
+	}
+
+	const firework = new Firework(5);
 	function animate () {
 		context.clearRect(0,0,canvas.width,canvas.height)
-		centerX *= 1.1;
-		drawCircle(centerX);
+		firework.draw();
 		requestAnimationFrame(animate)
 	}
 	animate();
