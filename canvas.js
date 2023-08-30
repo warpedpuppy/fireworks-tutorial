2ca849bbf4a35c2c5df9c5a2e9ab4e1c45bb5b62
ted <ted@warpedpuppy.com>
2023-08-30 08:39:31 -0400

    added size variance and multiple fws

--- canvas.js
+++ canvas.js
@@ -8,8 +8,21 @@
 	canvas.height = 300;
 	const context = canvas.getContext('2d');
 
+	class Background {
+		constructor () {
+			
+		}
+		draw() {
+			context.beginPath();
+			context.rect(0,0,canvas.width, canvas.height);
+			context.fillStyle = `rgba(0,0,0,1)`;
+			context.fill();
+
+		}
+	}
+
 	class Firework {
-		beamQ = 10;
+		beamQ = 20;
 		beams = [];
 		centerX = canvas.width / 2;
 		centerY = canvas.height / 2;
@@ -18,20 +31,39 @@
 		buffer = 50;
 		increase;
 		alpha = 1;
+		radiusIncrease = this.returnRadiusIncrease();
 		maxRadius = Math.max(canvas.height, canvas.width) / 2;
 		constructor(radius) {
 			this.radius = radius;
+			this.color = this.returnColor();
 			for (let i = 0; i < this.beamQ; i++) {
 				let beam = new Beam(this.radius);
 				this.beams.push(beam);
 			}
 		}
 
-		returnXY(radius, angle) {
-			return {x: this.centerX + radius * Math.cos( this.angle ), y: this.centerY + radius * Math.sin( angle ) }
+		returnRadiusIncrease() {
+			return (Math.random() * 0.1) + 0.05;;
+		}
+
+		returnXY() {
+			return {
+				x: this.centerX + this.radius * Math.cos( this.angle ), 
+				y: this.centerY + this.radius * Math.sin( this.angle ) 
+			}
+		}
+
+		returnColor () {
+			let arr = [];
+			for (let i = 0; i < 3; i++) {
+				arr.push(Math.floor(Math.random() * 255))
+			}
+			return arr;
 		}
 
 		reset() {
+			this.radiusIncrease = this.returnRadiusIncrease();
+			this.color = this.returnColor();
 			this.angle = 0;
 			this.radius = 0;
 			this.alpha = 1;
@@ -43,10 +75,10 @@
 			for (let i = 0; i < this.beamQ; i++) {
 				let increase = (Math.PI * 2) / this.beamQ;
 				this.angle += increase;
-				let { x, y } = this.returnXY(this.radius, this.angle);
-				this.beams[i].draw(x, y, this.alpha);
-				this.alpha *= 0.997;
-				this.radius += 0.5;
+				let { x, y } = this.returnXY();
+				this.beams[i].draw(x, y, this.alpha, this.color);
+				this.alpha *= 0.999;
+				this.radius += this.radiusIncrease;
 				if (this.radius > Math.max(canvas.width, canvas.height) / 2) {
 					this.reset();
 				}
@@ -56,25 +88,34 @@
 
 	class Beam {
 
-		color = [255,0,255];
 		size;
-		radius = (Math.random() * 0.5) ;
+		sizeVariance = (Math.random() * 0.3) + 0.7;
 		constructor (size) {
 			this.size = size;
 		}
 
-		draw(x, y, alpha) {
+		alter () {
+			this.sizeVariance = (Math.random() * 0.3) + 0.7;
+		}
+
+		draw(x, y, alpha, color) {
 			context.beginPath();
-			context.arc(x, y, this.size, 0, 2 * Math.PI, false);
-			context.fillStyle = `rgba(${this.color[0]}, ${this.color[1]}, ${this.color[2]}, ${alpha})`;
+			context.arc(x * this.sizeVariance, y * this.sizeVariance, this.size, 0, 2 * Math.PI, false);
+			context.fillStyle = `rgba(${color[0]}, ${color[1]}, ${color[2]}, ${alpha})`;
 			context.fill();
 		}
 	}
 
-	const firework = new Firework(5);
+	const firework = new Firework(3);
+	const firework2 = new Firework(5);
+	const firework3 = new Firework(1);
+	const background = new Background();
 	function animate () {
-		context.clearRect(0,0,canvas.width,canvas.height)
+		context.clearRect(0,0,canvas.width,canvas.height);
+		background.draw();
 		firework.draw();
+		firework2.draw();
+		firework3.draw();
 		requestAnimationFrame(animate)
 	}
 	animate();
@@ -82,59 +123,5 @@
 
 
 
-	// const canvas = document.getElementById('canvas');
-	// canvas.width = 600;
-	// canvas.height = 300;
-	// const context = canvas.getContext('2d');
-	// let centerX =  canvas.width / 2;
-	// let speed = 10;
-	// let deacceleration = 0.95;
-	// let centerY = canvas.height / 2;
-	// let radius = 5;
-	// let alpha = 1;
-	// let beamQ = 10;
-	// let angle = 0, increase;
-	
-	// drawCircle(centerX)
-
-	// function drawCircle(centerX, centerY, alpha) {
-	// 	context.beginPath();
-	// 	context.arc(centerX, centerY, radius, 0, 2 * Math.PI, false);
-	// 	context.fillStyle = `rgba(0, 0, 0, ${alpha})`;
-	// 	context.fill();
-	// }
-
-	// function returnXY(radius, angle) {
-	// 	return {x: (canvas.width / 2) + radius * Math.cos( angle ), y: (canvas.height / 2) + radius * Math.sin( angle ) }
-	// }
-
-
-
-	// function animate () {
-	// 	context.clearRect(0,0,canvas.width,canvas.height)
-		
-	// 	speed *= deacceleration;
-	// 	alpha *= deacceleration;
-		
-		
-	// 	for (let i = 0; i < beamQ; i++) {
-	// 		centerX += speed;
-	// 		increase = (Math.PI * 2) / beamQ;
-	// 		angle += increase;
-	// 		let { x, y } = returnXY(radius, angle);
-	// 		drawCircle(x, y, alpha);
-	// 	}
-
-	// 	radius ++;
-
-	// 	if (speed < 0.1) {
-	// 		speed = 8;
-	// 		alpha = 1;
-	// 		radius = 0;
-	// 	}
-
-	// 	requestAnimationFrame(animate)
-	// }
-	// animate();
 
 })()
